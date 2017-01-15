9a5b28955f4c6c9eb42284d1e10ce26443084e55
Pol Rodoreda <pol_rodoreda@hotmail.com>
2017-01-15 20:15:49 +0100

    Delete old files

--- yachtNN.R
+++ yachtNN.R
@@ -4,28 +4,26 @@ setwd("~/Documents/Universitat/Màster/Q1/CSI/Machine Learning/Project")
 #[TO DO] Try to normalize data before training neural network
 
 #Read dataset
-yacht.data <- read.table(file = "yacht_hydrodynamics.data")
-names(yacht.data) <- c('LonPos', 'PrismCoeff', 'LenDisRatio', 'BeamDRatio', 'LenBRatio', 'FroudNum', 'Resistance')
-N <- length(yacht.data)
+data <- read.table(file = "yacht_hydrodynamics.data")
+names(data) <- c('LonPos', 'PrismCoeff', 'LenDisRatio', 'BeamDRatio', 'LenBRatio', 'FroudNum', 'Resistance')
+N <- nrow(data)
 
-#Scaling dataset
-maxs <- apply(yacht.data, 2, max) 
-mins <- apply(yacht.data, 2, min)
-scaled <- as.data.frame(scale(yacht.data, center = mins, scale = maxs - mins))
+#Scaling data
+data <- scale(data)
+data <- as.data.frame(data)
 
 #Adding train column
 set.seed(1)
-yacht.data["train"] <- NA
-yacht.data$train <- sample(c('T', 'V'), size = N, replace = TRUE, prob = c(0.7, 0.3))
-
-#Train and test data
-train.data <- yacht.data[yacht.data$train == 'T',]
-validation.data <- yacht.data[yacht.data$train == 'V',]
+learn <- sample(1:N, round(2*N/3))
+nlearn <- length(learn)
+ntest <- N - nlearn
+data.learn <- data[learn,]
+data.test <- data[-learn,]
 
 library(neuralnet)
 
 #Neural networn training
-nn <- neuralnet(Resistance ~ LonPos + PrismCoeff + LenDisRatio + BeamDRatio + LenBRatio + FroudNum, data = train.data, hidden = 5, rep = 3, threshold = 0.1, stepmax = 1e7)
+nn <- neuralnet(Resistance ~ LonPos + PrismCoeff + LenDisRatio + BeamDRatio + LenBRatio + FroudNum, data = data.learn, hidden = 10, rep = 3, threshold = 0.1, stepmax = 1e7)
 plot(nn, rep = 'best')
 print(nn)
 
@@ -39,16 +37,22 @@ gwplot(nn, selected.covariate = 'LenBRatio', rep = 'best')
 gwplot(nn, selected.covariate = 'FroudNum', rep = 'best')
 
 #Neural network validation
-output <- compute(nn, validation.data[1:6], rep = 1)
-result <- data.frame(Real = validation.data$Resistance, Predicted = output$net.result, Error = (validation.data$Resistance - output$net.result)/validation.data$Resistance)
+output <- compute(nn, data.test[1:6], rep = 1)
+result <- data.frame(Real = data.test$Resistance, Predicted = output$net.result, Error = (data.test$Resistance - output$net.result)/data.test$Resistance)
 
 par(mfrow=c(1,1))
-plot(validation.data$Resistance, col = 'red', ylab = 'Resistance', main = 'Real vs Prediction')
+plot(data.test$Resistance, col = 'red', ylab = 'Resistance', main = 'Real vs Prediction')
 points(output$net.result, col = 'blue')
 legend('topright', legend=c('Real','NN'), pch=18, col=c('red','blue'))
 
 plot(result$Error, ylab = 'Error', main = 'Neural Network error', type = 'l', col = 'black')
 
-mean(result$Error)
+nn.predictions <- predict(nn)
+mean.square.error <- sum((data.learn$Resistance - rf.predictions)^2)/nlearn
+mean.square.error*100
+
+nn.predictions <- predict(nn, newdata=data.test)
+mean.square.error <- sum((data.test$Resistance - nn.predictions)^2)/ntest
+mean.square.error*100
 
 #[TO DO] How to calculate neural network accuracy
